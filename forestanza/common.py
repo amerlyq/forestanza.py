beaabfa3c603d9814324bda519fb2785c65ae73c
Amerlyq <amerlyq@gmail.com>
2015-11-07 16:45:31 +0200

    RFC dict_flatten by yield generator

--- forestanza/common.py
+++ forestanza/common.py
@@ -3,23 +3,31 @@
 KEYDEF = '_'
 def dict_resolve(obj, chain, i=0):
     if not isinstance(obj, dict) or i >= len(chain):
-        return chain[:i]  # i -- количество удачно пройденных звеньев
+        return (chain[:i], obj)
     if chain[i] in obj:
         ret = dict_resolve(obj[chain[i]], chain, i+1)
         if ret is not None:
             return ret
     if KEYDEF in obj:
-        return chain[:i] + [KEYDEF]
+        return (chain[:i] + [KEYDEF], obj[KEYDEF])
 # THINK could be optimized by moving on next step i -> i+1
 # and starting from i=1
 # return dict_resolve(obj, chain, i+1)
 
 
+# WARNING: need Python >= 3.3
+def dict_flatten(obj, chain=[]):
+    for k, v in sorted(obj.items()):
+        if isinstance(v, dict):
+            yield from dict_flatten(v, chain + [k])
+        else:
+            yield (chain + [k], v)
+
+
 def dict_intersect(obj, mask):
-    if not isinstance(obj, dict):
+    if not isinstance(obj, dict) or not isinstance(mask, dict):
         return obj
     return {k: v if KEYDEF == k else dict_intersect(v, mask[k])
             for k, v in obj.items()
             if KEYDEF == k or k in mask}
 # { k: v['id1'] for k, v in a.items() if 'id1' in v }
-
--- tests/common_test.py
+++ tests/common_test.py
@@ -1,42 +1,56 @@
 from forestanza.common import (
-    KEYDEF, dict_resolve, dict_intersect)
+    KEYDEF, dict_resolve, dict_flatten, dict_intersect)
 
 DATA = {KEYDEF: 1, 'a': 2, 'b': {'ib': 5},
         'c': {KEYDEF: 3, 'ic': 4, 'jc': {'m': 1}}}
 
+def chain_resolve(*chain):
+    return dict_resolve(DATA, list(chain))[0]
+
 class TestF_resolve_chain:
     def test_1L(self):
-        assert [] == dict_resolve(DATA, [])
-        assert ['a'] == dict_resolve(DATA, ['a'])
-        assert ['b'] == dict_resolve(DATA, ['b'])
-        assert ['c'] == dict_resolve(DATA, ['c'])
-        assert [KEYDEF] == dict_resolve(DATA, ['z'])
+        assert [] == chain_resolve()
+        assert ['a'] == chain_resolve('a')
+        assert ['b'] == chain_resolve('b')
+        assert ['c'] == chain_resolve('c')
+        assert [KEYDEF] == chain_resolve('z')
 
     def test_2L(self):
-        assert ['a'] == dict_resolve(DATA, ['a', 'z'])
-        assert [KEYDEF] == dict_resolve(DATA, ['b', 'z'])
-        assert [KEYDEF] == dict_resolve(DATA, ['y', 'z'])
-        assert ['b', 'ib'] == dict_resolve(DATA, ['b', 'ib'])
-        assert [KEYDEF] == dict_resolve(DATA, ['b', 'z'])
-        assert ['c', 'ic'] == dict_resolve(DATA, ['c', 'ic'])
-        assert ['c', KEYDEF] == dict_resolve(DATA, ['c', 'z'])
+        assert ['a'] == chain_resolve('a', 'z')
+        assert [KEYDEF] == chain_resolve('b', 'z')
+        assert [KEYDEF] == chain_resolve('y', 'z')
+        assert ['b', 'ib'] == chain_resolve('b', 'ib')
+        assert [KEYDEF] == chain_resolve('b', 'z')
+        assert ['c', 'ic'] == chain_resolve('c', 'ic')
+        assert ['c', KEYDEF] == chain_resolve('c', 'z')
 
     def test_3L(self):
-        assert ['a'] == dict_resolve(DATA, ['a', 'y', 'z'])
-        assert ['b', 'ib'] == dict_resolve(DATA, ['b', 'ib', 'z'])
-        assert [KEYDEF] == dict_resolve(DATA, ['b', 'y', 'z'])
-        assert ['c', 'ic'] == dict_resolve(DATA, ['c', 'ic', 'z'])
-        assert ['c', KEYDEF] == dict_resolve(DATA, ['c', 'y', 'z'])
-        assert ['c', 'jc', 'm'] == dict_resolve(DATA, ['c', 'jc', 'm'])
-        assert ['c', KEYDEF] == dict_resolve(DATA, ['c', 'jc', 'z'])
+        assert ['a'] == chain_resolve('a', 'y', 'z')
+        assert ['b', 'ib'] == chain_resolve('b', 'ib', 'z')
+        assert [KEYDEF] == chain_resolve('b', 'y', 'z')
+        assert ['c', 'ic'] == chain_resolve('c', 'ic', 'z')
+        assert ['c', KEYDEF] == chain_resolve('c', 'y', 'z')
+        assert ['c', 'jc', 'm'] == chain_resolve('c', 'jc', 'm')
+        assert ['c', KEYDEF] == chain_resolve('c', 'jc', 'z')
 
 
-MASK = {'a': [1, 2], 'c': {'ic': [3, 4], 'ff': 5}, 'f': [6]}
-
-class TestF_dict_intersect:
+class TestF_dict_flatten:
     def test_1L(self):
-        assert {'_': 1} == dict_intersect(DATA, {})
-        assert {'_': 1, 'a': 2} == dict_intersect(DATA, {'a': 0})
-        assert {'_': 1, 'c': {'_': 3}} == dict_intersect(DATA, {'c': 0})
-        assert {'_': 1} == dict_intersect(DATA, {'b': 0})
-        assert {'_': 1} == dict_intersect(DATA, {'z': 0})
+        assert [] == list(dict_flatten({}))
+        assert [(['a'], 1)] == list(dict_flatten({'a': 1}))
+        assert ([(['a'], 1), (['b'], 2)] ==
+                list(dict_flatten({'a': 1, 'b': 2})))
+
+    def test_2L(self):
+        assert ([(['a', 'c'], 3), (['a', 'd'], 4)] ==
+                list(dict_flatten({'a': {'c': 3, 'd': 4}})))
+
+
+# MASK = {'a': [1, 2], 'c': {'ic': [3, 4], 'ff': 5}, 'f': [6]}
+# class TestF_dict_intersect:
+#     def test_1L(self):
+#         assert {'_': 1} == dict_intersect(DATA, {})
+#         assert {'_': 1, 'a': 2} == dict_intersect(DATA, {'a': 0})
+#         assert {'_': 1, 'c': {'_': 3}} == dict_intersect(DATA, {'c': 0})
+#         assert {'_': 1} == dict_intersect(DATA, {'b': 0})
+#         assert {'_': 1} == dict_intersect(DATA, {'z': 0})
