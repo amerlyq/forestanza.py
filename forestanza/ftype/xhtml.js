213bec71960a7b9134bc90488f1af0ca00d8bc1e
Amerlyq <amerlyq@gmail.com>
2016-02-05 13:28:04 +0200

    Chg: simplified js, sessionStorage; Fix: scroll save

--- forestanza/ftype/xhtml.js
+++ forestanza/ftype/xhtml.js
@@ -3,41 +3,24 @@ function storePos() {
     var y = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop  || 0);
     var h = (document.height || document.body.offsetHeight);
     var pos = y / h;
-    localStorage.setItem('scrollRelPos', pos);
+    sessionStorage.setItem('scrollRelPos', pos);
 }
 
 function loadPos() {
-    var pos = localStorage.getItem('scrollRelPos') || 0;
+    var pos = sessionStorage.getItem('scrollRelPos');
+    if (typeof pos == 'undefined') { return; }
     var h = (document.height || document.body.offsetHeight);
-    var y = pos * h;
+    var y = Math.round(pos * h);
     window.scrollTo(0, y);
 }
 
-function updatePos() {
-    doUpdatePos = true;
-    storePos();
-}
-
-function onResizePost() { loadPos(); }
-function onResize() {
-    if (this.resizeTimeout) { clearTimeout(this.resizeTimeout); }
-    this.resizeTimeout = setTimeout(onResizePost, 500);
-}
 function onScroll() {
-    if (doUpdatePos) {
-        doUpdatePos = false;
-        if (this.scrollTimeout) {
-            clearTimeout(this.scrollTimeout);
-            this.scrollTimeout = null;
-        }
-        this.scrollTimeout = setTimeout(updatePos, 500);
-    }
+    if (this.scrollTimeout) { clearTimeout(this.scrollTimeout); }
+    this.scrollTimeout = setTimeout(storePos, 500);
 }
 
 (function () {  // Main
-    var doUpdatePos = true;
-    window.onload = function () { loadPos(); };
-    window.onbeforeunload = function (e) { storePos(); };
-    window.onresize = onResize;
+    window.onbeforeunload = storePos;
+    window.onresize = loadPos;
     window.onscroll = onScroll;
 })();
