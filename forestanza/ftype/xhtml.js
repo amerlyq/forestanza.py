7f5eb3d9d9a452b95e24f96b0c5084162209a567
Amerlyq <amerlyq@gmail.com>
2016-07-07 06:42:25 +0300

    Fix: individual scroll position per chapter

--- forestanza/ftype/xhtml.js
+++ forestanza/ftype/xhtml.js
@@ -1,13 +1,17 @@
+var scrollStorageKey = window.location.pathname;
+var scrollStorageKey = 'scrpos-' + scrollStorageKey.substring(
+    scrollStorageKey.lastIndexOf("/")+1, scrollStorageKey.lastIndexOf("."));
+
 function storePos() {
     var doc = document.documentElement;
     var y = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop  || 0);
     var h = (document.height || document.body.offsetHeight);
     var pos = y / h;
-    localStorage.setItem('scrollRelPos', pos);
+    localStorage.setItem(scrollStorageKey, pos);
 }
 
 function loadPos() {
-    var pos = localStorage.getItem('scrollRelPos');
+    var pos = localStorage.getItem(scrollStorageKey);
     if (typeof pos == 'undefined') { return; }
     var h = (document.height || document.body.offsetHeight);
     var y = Math.round(pos * h);
